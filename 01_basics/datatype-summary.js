a6ef3ed9a892b60ba3f831f844b2725d0180749f
dipakvidani <dipakvidani123@gmail.com>
2025-01-22 13:52:34 +0000

    stack&heap memory allocation

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,22 @@
+// Data Type 
+
+// premitive data type
+//string,number,Boolean,BigInt,null,undefined,symbol
+
+// let sym1=Symbol("foo")
+// console.log(sym1)
+
+// Non-Premitive(Refrance) Datatype
+//Array,Object,
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    // Memory
+
+    // primitive------>Stack 
+    // =========       ======
+    // 
+    
+    // non-primitive------->Heap    //array,object
+    // =============       ======
