1fc4d944cb463eee1c5e3c8f89f2e65699a36549
dipakvidani <dipakvidani123@gmail.com>
2025-01-22 05:17:26 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+console.log( 2 + 3);
+console.log( 2 - 3);
+console.log( 2 * 3);
+console.log( 2 / 3);
+console.log( 2 ** 3);
+
+console.log("dipak");
+
+
+console.log(null==0)    //false
+console.log(null>0)     //false
+console.log(null<0)     //false
+console.log(null>=0)    //true
+console.log(null<=0)    //true
+
+
+
+console.log(undefined>0)     //false
+console.log(undefined<0)     //false
+console.log(undefined>=0)    //false
+console.log(undefined<=0)    //false
+
+
